f5e1aebe74d63d071b5f2c50314b0a7b0a3caa81
Vince Williams <williamsvm20@gmail.com>
2020-09-15 17:37:48 -0400

    linear and binary search are passing tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,11 @@
 def linear_search(arr, target):
     # Your code here
+    i = 0
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
+        else:
+            i += 1
 
 
     return -1   # not found
@@ -9,6 +15,15 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    high = len(arr) - 1
+    low = 0
+    while low <= high:
+        midpoint = (low + high) // 2
+        if arr[midpoint] > target:
+            high = midpoint - 1
+        elif arr[midpoint] < target:
+            low = midpoint + 1
+        else:
+            return midpoint
 
     return -1  # not found
